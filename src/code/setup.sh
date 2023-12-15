fec09e4440b0bf6a79e29995c08344aa49d3e165
Davide <davidecoccia@gmail.com>
2023-12-15 21:18:19 +0100

    Update setup.sh

--- src/code/setup.sh
+++ src/code/setup.sh
@@ -41,7 +41,8 @@ zip -r pyzbar_layer.zip python && rm -rf python && rm -rf zbar
 
 #package lambda function code in a .zip
 zip -r lambda_function.zip Barcode-QR-Decoder-Lambda/src/code/lambda_function.zip
-aws s3 sync . s3://$BUCKET_NAME/BarcodeQRDecoder/qr-reader/assets --exclude="*" --include="*layer.zip" --include="lambda_function.zip"
+#aws s3 sync . s3://$BUCKET_NAME/BarcodeQRDecoder/qr-reader/assets --exclude="*" --exclude=".c9*" --include="*layer.zip" --include="lambda_function.zip"
+aws s3 sync . s3://$BUCKET_NAME/BarcodeQRDecoder/qr-reader/assets --include="*.zip"
 
 #delete generated lambda layers after uploaded to S3 to clean curent directory
 rm pillow_layer.zip pyzbar_layer.zip lambda_function.zip
